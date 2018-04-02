3e67690da2f393d24e48f8a321e4797ee0823fac
Ji Yim <gsooyim@gmail.com>
2018-04-02 21:17:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " || "" || nil || !"X" || !"O"
+  if board[index] == " " || "" || nil
     return false
+  elsif board[index] == "X"
+    return true
   end
 end
