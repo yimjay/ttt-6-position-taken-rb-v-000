090afadd31cf9502b72102a80b6d0360b0d1c052
Ji Yim <gsooyim@gmail.com>
2018-04-02 21:15:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
   if board[index] == "X" || "O"
     return true
   end
-  if board[index] == " " || "" | nil
+  if board[index] == " " || "" || nil
     return false
   end
 end
